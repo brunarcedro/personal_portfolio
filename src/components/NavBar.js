9d2955cf8e098d243a988e3a2c18b1ec91233130
Bruna Cedro <134100052+brunarcedro@users.noreply.github.com>
2024-05-09 09:19:36 -0300

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -49,9 +49,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.linkedin.com/in/brunarcedro/"><img src={navIcon1} alt="" /></a>
                 <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.instagram.com/brucodes/"><img src={navIcon3} alt="" /></a>
               </div>
              
               <button className="vvd">
